18dd19e39a440a8e384d8b9d7cd931a8ee7223ba
milagros Yemira <milagreishon.12@gmail.com>
2021-12-14 09:59:52 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,53 +1,45 @@
 let nombre = document.getElementById("nombre");
 let forma = document.getElementById("forma");
-let color= document.getElementById("color");
-let agregar_btn = document.getElementById("agregar-btn");
-let caja = document.getElementById("datos");
-let nombres = [];
+let color = document.getElementById("color");
 
-let datos = JSON.parse(localStorage.getItem("nombres"));
+let save_btn = document.getElementById("save-btn");
 
-if (datos != null) {
-    nombres = datos;
-}
-console.log(forma.value);
-console.log(color.value)
+let contenedor = document.getElementById("figuras");
 
-agregar_btn.addEventListener("click", () => {
-    let nom = nombre.value;
-    nombres.push({
-       nombres: nom,
+let figuras =[];
+
+save_btn.addEventListener("click" , () => {
+    figuras.push({
+        nombre: nombre.value,
+        forma: forma.value,
+        color: color.value,
     });
-    localStorage.setItem("nombres", JSON.stringify(nombres));
-    nombre.value = "";
-
-    let form = forma.value;
-    forma.push({
-       formas: form,
-    });
-    localStorage.setItem("formas", JSON.stringify(forma));
-    forma.value = "";
-
-    let color = color.value;
-    color.push({
-       color: color,
-    });
-    localStorage.setItem("color", JSON.stringify(color));
-    color.value = "";
-    mostrar_lista();
+    pintar_figuras();
 });
- 
+
+const pintar_figuras = () => {
+    contenedor.innerHTML = "";
+    figuras.forEach((e) => {
+        contenedor.insertAdjacentHTML(
+            "beforeend",
+            `
+                    <div class="${e.forma}" style="background;${e.color}">
+                      ${e.nombre}
+                    </div>
+              `
+        );
+    });
+};
 const mostrar_lista = () => {
     caja.innerHTML = "";
-    nombres.forEach((element) => {
+    nombre.forEach((element) => {
         caja.insertAdjacentHTML(
             "beforeend",
             `
-                    <li>${element.nombres}</li>
+                    <li>${element.nombre}</li>
               `
         );
     });
     
 
 };
-mostrar_lista();
